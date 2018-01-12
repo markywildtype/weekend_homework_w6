9b579d56a6f15f269dec246312bfb40229d3364d
Mark Blanford <markytorture@hotmail.com>
2018-01-12 11:50:48 +0000

    Added Dining Room class.

---
+++ src/main/java/rooms/DiningRoom.java
@@ -0,0 +1,8 @@
+package rooms;
+
+public class DiningRoom extends Room {
+
+    public DiningRoom(int floor, int roomNumber) {
+        super(floor, roomNumber);
+    }
+}
---
+++ src/test/java/DiningRoomTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import rooms.DiningRoom;
+import rooms.Occupied;
+
+import static org.junit.Assert.assertEquals;
+
+public class DiningRoomTest {
+
+    DiningRoom diningRoom;
+
+    @Before
+    public void before(){
+        diningRoom = new DiningRoom(0, 5);
+    }
+
+    @Test
+    public void startsUnoccupied(){
+        assertEquals(Occupied.UNOCCUPIED, diningRoom.getOccupiedStatus());
+    }
+}
